3596707789c14f16b82914cf3e27da99af2b0001
samidbb <samdi@dfds.com>
2023-08-08 12:09:17 +0200

    test terraform file

---
+++ terraform/main.tf
@@ -0,0 +1,61 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.74.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-1"
+
+  skip_credentials_validation = true
+  skip_requesting_account_id  = true
+  skip_metadata_api_check     = true
+  s3_force_path_style         = true
+  access_key                  = "mock_access_key"
+  secret_key                  = "mock_secret_key"
+}
+
+resource "aws_rds_cluster" "default" {
+  cluster_identifier      = "aurora-cluster-demo"
+  engine                  = "aurora-mysql"
+  engine_version          = "5.7.mysql_aurora.2.03.2"
+  availability_zones      = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  database_name           = "mydb"
+  master_username         = "foo"
+  master_password         = "bar"
+  backup_retention_period = 5
+  preferred_backup_window = "07:00-09:00"
+  tags = var.tags_group1
+}
+
+resource "aws_rds_cluster" "default2" {
+  cluster_identifier      = "aurora-cluster-demo2"
+  engine                  = "aurora-mysql"
+  engine_version          = "5.7.mysql_aurora.2.03.2"
+  availability_zones      = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  database_name           = "mydb"
+  master_username         = "foo"
+  master_password         = "bar"
+  backup_retention_period = 5
+  preferred_backup_window = "07:00-09:00"
+  # tags = var.tags_group2
+}
+
+
+variable "tags_group1" {
+  type = map(string)
+  default = {
+    "dfds.env" = "prod"
+    "Custodian" = "true"
+  }
+}
+
+variable "tags_group2" {
+  type = map(string)
+  default = {
+    "dfds.env" = "dev"
+  }
+}
